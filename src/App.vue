1d35a3b53c62d5fc29284bb65501a4fbb4da0d88
António Gonçalves <59710384+Cyb3rWolf945@users.noreply.github.com>
2025-03-21 14:13:07 +0000

    update project
    project base

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { ref } from 'vue';
+import { RouterView } from 'vue-router';
+import { Button } from '@/components/ui/button'
+
+
+const data = ref([
+  { text: "hello" },
+  { text: "to" },
+  { text: "you!!!" },
+  {text: "fjdfjhbbje"}
+]);
 </script>
 
 <template>
+
+  
+<h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+
+  <div v-for="(item, index) in data" :key="index">
+    <input type="text" v-model="item.text">
+  </div>
+
+  <Button>Button</Button>
+
+  {{ data }}
   <RouterView />
 </template>
-
-<style scoped>
-
-</style>
